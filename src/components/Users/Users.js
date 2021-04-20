045b5051d8d4704ecea39151e2906ed967f9412d
sultan <email>
2021-04-20 22:21:35 +0600

    add Get-request and refactoring UsersPage

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,47 +1,15 @@
 import React from 'react'
 import classes from './Users.module.css'
+import * as axios from 'axios'
+import defaultAva from '../../img/1.jpg'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    avaUrl: 'https://media-exp1.licdn.com/dms/image/C5622AQH32m5gDbnn5g/feedshare-shrink_800/0/1601896044130?e=1619654400&v=beta&t=xX8SiRZbgV1JLpYYs6bcdQrB9cIWlC0J20DUX3KZPTU',
-                    followed: true,
-                    fullName: 'Sultan',
-                    status: 'I am a boss',
-                    location: { city: 'Bishkek', country: 'Kyrgzstan' }
-                },
-                {
-                    id: 2,
-                    avaUrl: 'https://videourokiprogrammirovanie.ru/upload/000/u0/0/c/0c5e7750.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am looking for a Job right now',
-                    location: { city: 'Moscow', country: 'Russia' }
-                },
-                {
-                    id: 3,
-                    avaUrl: 'https://static-cdn.jtvnw.net/jtv_user_pictures/95f080c5-d5a9-4659-944c-091b7a2bb91c-profile_image-300x300.png',
-                    followed: false,
-                    fullName: 'Ilya',
-                    status: 'I like a football!!!',
-                    location: { city: 'Kiev', country: 'Ukraine' }
-                },
-                {
-                    id: 4,
-                    avaUrl: 'https://ae01.alicdn.com/kf/HTB1cQupitnJ8KJjSszdq6yxuFXaK/-.jpg',
-                    followed: true,
-                    fullName: 'Kiyoko',
-                    status: 'I am so pretty',
-                    location: { city: 'Tokyo', country: 'Japan' }
-                },
-            ]
-        )
+
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => props.setUsers(response.data.items))
     }
-
-
 
     return (
         <div className={classes.users}>
@@ -51,25 +19,23 @@ let Users = (props) => {
                     <div className={classes.user} key={user.id}>
                         <span className={classes.leftBlock}>
                             <div className={classes.userAva}>
-                                <img src={user.avaUrl} alt="" />
+                                <img src={user.photos.small !== null ? user.photos.small : defaultAva} alt="" />
                             </div>
-                            <div>
-                                {user.followed ?
-                                    <button onClick={() => props.unfollow(user.id)} className={classes.btn}>Unfollow</button>
-                                    :
-                                    <button onClick={() => props.follow(user.id)} className={classes.btn}>Follow</button>
-                                }
-                            </div>
+                            {user.followed ?
+                                <button onClick={() => props.unfollow(user.id)} className={classes.btn}>Unfollow</button>
+                                :
+                                <button onClick={() => props.follow(user.id)} className={classes.btn}>Follow</button>
+                            }
                         </span>
 
                         <span className={classes.rightBlock}>
                             <span className={classes.about_user}>
-                                <div className={classes.userName}>{user.fullName}</div>
+                                <div className={classes.userName}>{user.name}</div>
                                 <div>{user.status}</div>
                             </span>
                             <span className={classes.location}>
-                                <div>{user.location.country}</div>
-                                <div>{user.location.city}</div>
+                                <div>{'user.location.country'}</div>
+                                <div>{'user.location.city'}</div>
                             </span>
                         </span>
                     </div>
