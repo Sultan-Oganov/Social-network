0a43894b655bf51ad141eb2cd400ee2e8de838fc
Sultan <sultan.oganov@gmail.com>
2021-09-09 03:45:01 +0600

    add local-State. Work with input-status

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -37,5 +37,5 @@ const mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps, { getUserProfile }),
     withRouter,
-    withAuthRedirect
+    // withAuthRedirect
 )(ProfileContainer)
--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Preloader from '../../common/Preloader/Preloader'
 import classes from './ProfileInfo.module.css'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
 
@@ -9,18 +10,21 @@ const ProfileInfo = (props) => {
     }
     return (
         <>
-            <h1 className={classes.title}>Main Content</h1>
+            {/* <h1 className={classes.title}>Main Content</h1>
             <img src="https://get-edu.kz/wp-content/uploads/2020/04/helpbox-contact.jpg" alt="wallpaper" />
-
-            <div>
+*/}
+            <div className={classes.descriptionBlock}>
                 <img
                     className={classes.userPhoto}
                     src={props.profile.photos.large}
                     alt="" />
-                <div className={classes.userName}>
-                    <b>Name:<em>{props.profile.fullName}</em></b>
-                </div>
+                <ProfileStatus status={'Hello MF'} />
             </div>
+            {/* 
+            <div className={classes.userName}>
+                <b>Name:<em>{props.profile.fullName}</em></b>
+            </div>
+        </div> */}
         </>
     )
 
